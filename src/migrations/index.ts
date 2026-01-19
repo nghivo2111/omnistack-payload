48013aede8037d27c12b4b7412d05bdef7950205
nghivo2111 <nghi.vo@omnistack.co>
2026-01-19 18:32:42 +0700

    Remove migration files for 20251231_055556

--- src/migrations/index.ts
+++
@@ -1,9 +0,0 @@
-import * as migration_20251231_055556 from './20251231_055556';
-
-export const migrations = [
-  {
-    up: migration_20251231_055556.up,
-    down: migration_20251231_055556.down,
-    name: '20251231_055556'
-  },
-];
